aaed7b1018c45e00da0bec8f62d4939f07176fca
misha-yu <misha.yu.reg@gmail.com>
2019-02-16 21:46:52 +0600

    react_node_heroku_app
    react_node_heroku_app

---
+++ build/precache-manifest.10d5f06cf90bb14b547c8a2f01659988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4545d55c6e379d16b582",
+    "url": "/static/css/main.f36bbe1a.chunk.css"
+  },
+  {
+    "revision": "4545d55c6e379d16b582",
+    "url": "/static/js/main.4545d55c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a4005b7a92db3c2cb4197cb4dcb79193",
+    "url": "/index.html"
+  }
+];
