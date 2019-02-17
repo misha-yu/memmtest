b2fd8c22ad3c809fb3f47771f3e4f7cd873f2033
misha-yu <misha.yu.reg@gmail.com>
2019-02-17 11:13:46 +0600

    select_all_products
    Added the connection to DB. And selected query from the table

--- server.js
+++ server.js
@@ -9,31 +9,29 @@ const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
 
 //const cors = require ('cors');
-const mysql = require('mysql'); //zzz
 
 const app = express();
 const dev = app.get('env') != 'production'
 
 
 // try to connect to database
-// const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products'; // this is a request to db, which I don't have
-// const connection = mysql.createConnection({
-//   host: 'eu-cdbr-west-02.cleardb.net',
-//   user: 'b21add02c8e9f2',
-//   password: '17bc1bca',
-//   database: 'react_sql',
-//   insecureAuth: true
-// });
+const connection = mysql.createConnection({
+  host: 'eu-cdbr-west-02.cleardb.net',
+  user: 'b21add02c8e9f2',
+  password: '17bc1bca',
+  database: 'heroku_f436156b9325bd8',
+  insecureAuth: true
+});
 
-// connection.connect( (err) => {
-//   console.log("trying to connect to DTB");
-//   if (err){
-//     console.log(">>> "+err);
-//     return err;
-//   }else{
-//     console.log('CONNECTED');
-//   }
-// });
+connection.connect( (err) => {
+  console.log("trying to connect to DTB");
+  if (err){
+    console.log(">>> "+err);
+    return err;
+  }else{
+    console.log('CONNECTED');
+  }
+});
 
 
 if (!dev) {
@@ -47,6 +45,26 @@ if (!dev) {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
 }
+
+app.get('/', (req,res) => {
+  res.send('go to /products to see products');
+});
+
+const SELECT_ALL_PRODUCTS_QUERY = 'SELECT * FROM products';
+app.get('/products', (req, res) => {
+  
+  connection.query(SELECT_ALL_PRODUCTS_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err)
+    }
+    else {
+      return res.json({
+        data: results
+      })
+    }
+  });
+  
+});
 
 if (dev){
   app.use(morgan('dev'));
